def385ca05587b15a2abbf3535bffd911c1cab6d
Rajix24 <rajix2100@gamil.com>
2026-09-21 12:23:02 +0000

    create 3 function that one get activity by id and one get activites by association

--- src/services/Activities.service.js
+++ src/services/Activities.service.js
@@ -1,36 +1,110 @@
 const db = require("../config/db").getPool()
 
 async function getAllActivites(){
-    const query = `SELECT
-        ac.id AS activity_id,
-        ac.name AS activity_name,
-        ac.base_price,
-        ac.max_capacity,
-        ac.age_category,
-        ac.requires_recent_certificate,
-        ac.day_of_week,
-        ac.start_time,
-        ac.end_time,
-        ac.sub_zone,
+    const query = `
+            SELECT
+                ac.id_activities AS activity_id,
+                ac.name_activities AS activity_name,
+                ac.base_price_activities AS base_price,
+                ac.max_capacity_activities AS max_capacity,
+                ac.age_category_activities AS age_category,
+                ac.requires_recent_certificate_activities AS requires_recent_certificate,
+                ac.day_of_week_activities AS day_of_week,
+                ac.start_time_activities AS start_time,
+                ac.end_time_activities AS end_time,
+                ac.sub_zone_activities AS sub_zone,
 
-        asco.name AS association_name,
-        a.name AS facility_name,
-        a.erp_capacity,
-        a.divisible
+                asso.name_associations AS association_name,
 
-        FROM activities AS ac
+                f.name_facilities AS facility_name,
+                f.erp_capacity_facilities AS erp_capacity,
+                f.divisible_facilities AS divisible
 
-        JOIN associations AS asco
-            ON ac.association_id = asco.id
+            FROM activities AS ac
 
-        JOIN facilities AS a
-            ON ac.facility_id = a.id;`
-    // const Activites = await db.query(query)
-    const Activities = ["yoga", "swimming", "basketball", "tennis", "soccer", "volleyball", "badminton", "table tennis", "martial arts", "dance"]
+            JOIN associations AS asso
+                ON ac.association_id_activities = asso.id_associations
+
+            JOIN facilities AS f
+                ON ac.facility_id_activities = f.id_facilities;
+            `
+    const Activities = await db.query(query)
     return Activities;
 }
 
+async function getActivityById(activityId) {
+    const query = `
+        SELECT
+            ac.id_activities AS activity_id,
+            ac.name_activities AS activity_name,
+            ac.base_price_activities AS base_price,
+            ac.max_capacity_activities AS max_capacity,
+            ac.age_category_activities AS age_category,
+            ac.requires_recent_certificate_activities AS requires_recent_certificate,
+            ac.day_of_week_activities AS day_of_week,
+            ac.start_time_activities AS start_time,
+            ac.end_time_activities AS end_time,
+            ac.sub_zone_activities AS sub_zone,
+
+            asso.name_associations AS association_name,
+
+            f.name_facilities AS facility_name,
+            f.erp_capacity_facilities AS erp_capacity,
+            f.divisible_facilities AS divisible
+
+        FROM activities AS ac
+
+        JOIN associations AS asso
+            ON ac.association_id_activities = asso.id_associations
+
+        JOIN facilities AS f
+            ON ac.facility_id_activities = f.id_facilities
+
+        WHERE ac.id_activities = $1;
+    `;
+
+    const result = await db.query(query, [activityId]);
+    return result.rows[0];
+}
+
+
+async function getActivitiesByAssociationId(associationId) {
+    const query = `
+        SELECT
+            ac.id_activities AS activity_id,
+            ac.name_activities AS activity_name,
+            ac.base_price_activities AS base_price,
+            ac.max_capacity_activities AS max_capacity,
+            ac.age_category_activities AS age_category,
+            ac.requires_recent_certificate_activities AS requires_recent_certificate,
+            ac.day_of_week_activities AS day_of_week,
+            ac.start_time_activities AS start_time,
+            ac.end_time_activities AS end_time,
+            ac.sub_zone_activities AS sub_zone,
+
+            asso.name_associations AS association_name,
+
+            f.name_facilities AS facility_name,
+            f.erp_capacity_facilities AS erp_capacity,
+            f.divisible_facilities AS divisible
+
+        FROM activities AS ac
+
+        JOIN associations AS asso
+            ON ac.association_id_activities = asso.id_associations
+
+        JOIN facilities AS f
+            ON ac.facility_id_activities = f.id_facilities
+
+        WHERE ac.association_id_activities = $1;
+    `;
+
+    const result = await db.query(query, [associationId]);
+    return result.rows;
+}
 
 module.exports = {
-    getAllActivites
+    getAllActivites,
+    getActivityById,
+    getActivitiesByAssociationId
 }
